c41bd26b7288a224caef35565c84c846bac820fd
Rosario Terranova <rosarioterranova91@gmail.com>
2020-01-03 13:16:51 +0100

    updated all codebase

--- latage.js
+++ latage.js
@@ -1,57 +1,33 @@
-//formula encoder
-function convertFormula (equation){
-	var res = encodeURIComponent(equation);
-	return "https://chart.googleapis.com/chart?cht=tx&chl="+res;
+let imgFormula = $("#imgFormula");
+let formulaInput = $("#formulaInput");
+let encodedEquation;
+let imgUrl;
+
+formulaInput.on("input",onInputEntered);
+$("#getUrlBtn").click(getUrl);
+$("#calcFormulaBtn").click(calculate);
+
+setFocusToInput();
+
+function onInputEntered(){
+    showImgFormula(formulaInput.val());
 }
 
-//call Google API for show image
-function showImgFormula(url) {
-	if(imgFormula.childNodes[0]!=null)
-		imgFormula.removeChild(imgFormula.childNodes[0]);
-	urlFormula = convertFormula(url);
-	var img = new Image();
-	img.src = urlFormula;
-	imgFormula.appendChild(img);
+function showImgFormula(equation){
+    encodedEquation = encodeURIComponent(equation);
+    imgUrl = "https://chart.googleapis.com/chart?cht=tx&chl="+encodedEquation;
+    imgFormula.attr("src",imgUrl);
 }
 
-//show url to API
-function showTxtFormula(url) {
-	//var span = document.getElementById('txtFormula');
-	urlFormula = convertFormula(url);
-	//span.textContent = urlFormula;
-	return urlFormula;
-}
-
-//clear al field
-function clearFormula(){
-	var inp = document.getElementById('formulaInput');
-	var im = document.getElementById('imgFormula');
-	var te = document.getElementById('txtFormula');
-	inp.value="";
-	im.removeChild(im.childNodes[0]);
-	//te.textContent = "";
-	setFocusToInput();
-}
-
-//On click button
-function setFormulaByButton(formula){
-	document.getElementById('formulaInput').value = document.getElementById('formulaInput').value+formula;
-	document.getElementById('formulaInput').oninput();
-	setFocusToInput();
-}
-
-//Copy to clipboard window
-function copyToClipboard(text) {
-    window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+function getUrl() {
+    window.prompt("Copy to clipboard: Ctrl+C, Enter", imgUrl);
   }
 
- //Copy to clipboard window
-function calculate(equation) {
-    var wolframUrl = "https://www.wolframalpha.com/input/?i=";
-    var res = encodeURIComponent(equation);
-    window.open(wolframUrl+res);
+function calculate() {
+    let wolframUrl = "https://www.wolframalpha.com/input/?i=";
+    window.open(wolframUrl+encodedEquation);
   }
 
-  function setFocusToInput(){
-    document.getElementById("formulaInput").focus();
+function setFocusToInput(){
+    formulaInput.focus();
 }
